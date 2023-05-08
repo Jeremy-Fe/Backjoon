a05f3e8c6b46517dbd23efa6a9bde4912e9b5ce4
신우철 <@DESKTOP-88QKH8S>
2023-05-08 18:13:45 +0900

    2023.05.08

---
+++ Backjoon/src/_2442_MultiFor5/Main.java
@@ -0,0 +1,21 @@
+package _2442_MultiFor5;
+
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int N = sc.nextInt();
+		sc.close();
+		
+		for (int i = 1; i <= N; i++) {
+			for (int j = i; j < N; j++) {
+				System.out.print(" ");
+			}
+			for (int j = 0; j < i * 2 - 1; j++) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+	}
+}
