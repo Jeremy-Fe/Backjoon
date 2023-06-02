deae8059edea1bb41a0bb221067cff62d9053d53
Shinwoocheol <Shinwoocheol@Shinwoocheol>
2023-06-03 00:18:45 +0900

    소중한 데이터

---
+++ Backjoon/src/_3046/Main.java
@@ -0,0 +1,14 @@
+package _3046;
+
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int R1 = sc.nextInt();
+		int S = sc.nextInt();
+		sc.close();
+		
+		System.out.println(S*2-R1);
+	}
+}
